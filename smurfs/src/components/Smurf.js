02239df1faac567dc429d679e5eceb643a9a1909
Dominic Torruellas <dom@domtorr.com>
2019-06-29 13:29:23 -0700

    hooked getSmurfs and state to App.js, rendering Smurf component

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Smurf = ({ name, age, height }) => {
+  return (
+    <div>
+      <div> {name} </div>
+      <div> {age} </div>
+      <div> {height} </div>
+    </div>
+  );
+};
+
+export default Smurf;
